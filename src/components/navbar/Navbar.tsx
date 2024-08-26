a85b29796f3b8969b529078e1ff5124e8c252a10
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-08-27 01:07:46 +0200

    chore: Add Navbar and Container components to layout

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Container from "../global/Container";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+import CartButton from "./CartButton";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className='border-b'>
+      <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-4'>
+        <Logo />
+        <NavSearch />
+        <div className='flex gap-4 items-center'>
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
+    </nav>
+  );
 }
