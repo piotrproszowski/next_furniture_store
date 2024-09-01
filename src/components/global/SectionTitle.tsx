0a7af46164a3d243c6157627c867336d447fa171
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-09-01 02:58:23 +0200

    feat: Add EmptyList and SectionTitle components for global use

---
+++ src/components/global/SectionTitle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Separator } from "../ui/separator";
+
+export default function SectionTitle({ text }: { text: string }) {
+  return (
+    <div>
+      <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
+}
