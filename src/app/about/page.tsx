d293b0d38d72fed48c65e446d61aac580734d0da
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-09-01 02:03:09 +0200

    chore: Update AboutPage to create and display test profiles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,21 @@ import db from "@/utils/db";
 import React from "react";
 
 export default async function AboutPage() {
-  const profile = await db.testProfile.create;
-  return <div>AboutPage</div>;
+  const profile = await db.testProfile.create({
+    data: {
+      name: "random name",
+    },
+  });
+  const users = await db.testProfile.findMany();
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <h2 key={user.id} className='text-2xl font-bold'>
+            {user.name}
+          </h2>
+        );
+      })}
+    </div>
+  );
 }
