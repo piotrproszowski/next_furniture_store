76bd2027226c35a196a793f7b5493b8df916b8f2
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-09-04 00:07:54 +0200

    feat: Add FeaturedProducts component for displaying featured products on the home page

---
+++ src/components/home/FeaturedProducts.tsx
@@ -0,0 +1,18 @@
+import { fetchFeaturedProducts } from "@/utils/actions";
+import React from "react";
+import EmptyList from "../global/EmptyList";
+import SectionTitle from "../global/SectionTitle";
+import ProductsGrid from "../products/ProductsGrid";
+
+export default async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+
+  if (products.length === 0) return <EmptyList />;
+
+  return (
+    <section className='pt-24'>
+      <SectionTitle text='Featured Products' />
+      <ProductsGrid products={products} />
+    </section>
+  );
+}
