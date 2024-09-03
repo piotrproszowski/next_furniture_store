2a18e906e55b6a2f230905e276a4fb47ede114a4
piotrproszowski <143588034+piotrproszowski@users.noreply.github.com>
2024-09-04 00:06:56 +0200

    chore: Update HomePage to use FeaturedProducts and Hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import Hero from "@/components/home/Hero";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1 className='text-3xl'>Home Page</h1>
-      <Button variant='outline' size='lg' className='capitalize m-8'>
-        Click me
-      </Button>
-    </div>
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
   );
 }
